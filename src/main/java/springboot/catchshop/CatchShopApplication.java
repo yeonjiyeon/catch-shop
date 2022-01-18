fa55d9eccc4e6c358bbefad6b5e514b38c05e741
kksswkd <kksswkd@gmail.com>
2022-01-18 19:51:19 +0900

    initial commit

---
+++ src/main/java/springboot/catchshop/CatchShopApplication.java
@@ -0,0 +1,13 @@
+package springboot.catchshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchShopApplication.class, args);
+	}
+
+}
