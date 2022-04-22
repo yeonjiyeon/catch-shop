f14639280a3cb460a623e800ba365d908972b237
yeonjiyeon <dalnim95@naver.com>
2022-04-22 18:56:18 +0900

    상품, 리뷰 테스트 추가

--- src/test/java/springboot/catchshop/service/ProductServiceTest.java
+++ src/test/java/springboot/catchshop/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package springboot.catchshop.service;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -46,11 +47,9 @@ public class ProductServiceTest {
     }
 
     @Test
-    public void 상품_목록_페이징(){
+    @DisplayName("상품 상태가 없을 때")
+    public void 상품_목록_페이징1(){
         //given
-        for(Long i = 201L; i< 300; i++){
-            Product.builder().id(i).build();
-        }
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
 
         //when
@@ -62,40 +61,60 @@ public class ProductServiceTest {
         };
     }
 
+    @Test
+    @DisplayName("상품 상태가 있을 때")
+    public void 상품_목록_페이징2(){
+        //given
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        //when
+        PageResultDTO<ProductDTO, Object[]> resultDTO = productService.readProductsWithProductStatus(pageRequestDTO, ProductStatus.BEST);
+
+        //then
+        for(ProductDTO productDTO : resultDTO.getDtoList()){
+            System.out.println("productDTO" + productDTO);
+        };
+    }
 
     @Test
     public void 상품_수정(){
         //given
+        Product product10001 = Product.builder().id(10002L).name("product1001").price(100000).stock(100).build();
+        Product saveProduct = productRepository.save(product10001);
+
+        Optional<Product> findProduct = productRepository.findById(saveProduct.getId());
         ProductDTO productDTO = ProductDTO.builder()
-            .id(10001L)
-            .name("product1001")
-            .price(100000)
-            .stock(100)
+            .id(findProduct.get().getId())
+            .name("Product10001L")
+            .price(findProduct.get().getPrice())
+            .stock(1000)
             .build();
 
 
         //when
-
+        productService.updateProduct(productDTO);
+        Optional<Product> findProduct2 = productRepository.findById(saveProduct.getId());
 
         //then
+        assertThat(findProduct2.get().getName()).isEqualTo("Product10001L");
+        assertThat(findProduct2.get().getStock()).isEqualTo(1000);
 
     }
 
     @Test
     public void 상품_삭제(){
         //given
-        ProductDTO productDTO = ProductDTO.builder()
-            .id(10001L)
-            .name("product1001")
-            .price(100000)
-            .stock(100)
-            .build();
+        Product product10001 = Product.builder().id(10002L).name("product1001").price(100000).stock(100).build();
+        Product saveProduct = productRepository.save(product10001);
+
+        Optional<Product> findProduct = productRepository.findById(saveProduct.getId());
 
         //when
-
+        productService.deleteProduct(findProduct.get().getId());
+        Optional<Product> findProduct2 = productRepository.findById(saveProduct.getId());
 
         //then
-
+        assertEquals(findProduct2, Optional.empty());
     }
     
     
